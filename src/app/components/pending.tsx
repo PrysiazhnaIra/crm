53620d1b81186c3d522de2bf2003b75ebbb8ccbc
Ira Prysiazhna <ira.prisyazhna@gmail.com>
2025-02-20 13:23:04 +0200

    status with tailwindcss

--- src/app/components/pending.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './pending.module.css';
-export interface PendingProps {
-  children: React.ReactNode; //цей проп дозволяє передавати будь-які дочірні елементи (string, JSX, інші компоненти тощо)
-}
-
-export default function Pending({ children }: PendingProps) {
-  return <span className={styles.label}>{children}</span>;
-}
