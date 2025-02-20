0f211f4bc218781eec3b76c9f84f00cfd22f0e9e
Ira Prysiazhna <ira.prisyazhna@gmail.com>
2025-02-20 17:16:00 +0200

    routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import { headers } from 'next/headers';
+// import { headers } from 'next/headers';
 import AddCompanyButton from './components/add-company-button';
 
 export default function Home() {
-  console.log(headers());
+  // console.log(headers());
   return (
     <main>
       <h1 className="text-3xl font-bold underline text-green-500">
         Hello, Next.js! This is a Home Page {new Date().toTimeString()}
       </h1>
-      <AddCompanyButton />{' '}
+      <AddCompanyButton />
     </main>
   );
 }
