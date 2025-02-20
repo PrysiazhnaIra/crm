914797f11b23c162f5ecd998c3aba73748e4e998
Ira Prysiazhna <ira.prisyazhna@gmail.com>
2025-02-20 14:59:06 +0200

    static and dynamic rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { headers } from 'next/headers';
 import StatusLabel, { Status } from './components/status-label';
 
 export default function Home() {
+  console.log(headers());
   return (
     <main>
       <h1 className="text-3xl font-bold underline text-green-500">
-        Hello, Next.js! This is a Home Page
+        Hello, Next.js! This is a Home Page {new Date().toTimeString()}
       </h1>
       <StatusLabel status={Status.Active}> Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
